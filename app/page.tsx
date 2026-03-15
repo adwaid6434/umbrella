17b0252db6d93324af4beb7006681cdfffb734f8
adwaid6434 <adwaid6434@gmail.com>
2026-03-15 16:04:39 +0530

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 // page.tsx
+"use client";
+import React from "react";
 import Hero from "@/components/Hero";
+
+import { Menu } from "lucide-react";
 import FeatureSection from "@/components/FeatureSection";
+import ProductShowcase from "@/components/ProductShowcase";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      {/* Move the Nav here (or into its own component) */}
       <header className="nav">
-        <div className="logo">ÆTHER.UMBRELLA.</div>
+        <div>
+          <span className={styles.logo}>ÆTHER.UMBRELLA.</span>
+          <Menu size={24} strokeWidth={1} />
+        </div>
+
         <div className="nav-links">
           <span>COLLECTION</span>
           <span>TECHNOLOGY</span>
@@ -20,6 +29,8 @@ export default function Home() {
 
       <Hero />
       <FeatureSection />
+      {/* The new page scrolls into view here */}
+      <ProductShowcase />
 
       {/* Move the fixed star here */}
       <div className="corner-star">✦</div>
